7ed30d4f8333ab7b896cb3c590a80067df3c568b
Emanuele Caruso <emanuele.caruso@jethr.com>
2026-01-22 10:59:06 +0100

    chore: making life easier when developing, but ts server sometimes crashes

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,65 +1,90 @@
 return {
-	"nvim-telescope/telescope.nvim",
-	version = false,
-	dependencies = {
-		"nvim-lua/plenary.nvim",
-		{ -- super fuzzy
-			"nvim-telescope/telescope-fzf-native.nvim",
-			build = "make",
-			cond = function()
-				return vim.fn.executable("make") == 1
-			end,
-		},
-	},
-	config = function()
-		local telescope = require("telescope")
-		local builtin = require("telescope.builtin")
-		local actions = require("telescope.actions")
-
-		telescope.setup({
-			defaults = {
-				mappings = {
-					i = {
-						["<C-h>"] = "which_key",
-					},
-				},
+	{
+		"nvim-telescope/telescope.nvim",
+		version = false,
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			{ -- super fuzzy
+				"nvim-telescope/telescope-fzf-native.nvim",
+				build = "make",
+				cond = function()
+					return vim.fn.executable("make") == 1
+				end,
 			},
-			pickers = {
-				buffers = {
-					sort_mru = true,
-					ignore_current_buffer = true,
+			"folke/trouble.nvim",
+		},
+		config = function()
+			local telescope = require("telescope")
+			local builtin = require("telescope.builtin")
+			local actions = require("telescope.actions")
+			local trouble = require("trouble.sources.telescope")
+
+			telescope.setup({
+				defaults = {
 					mappings = {
 						i = {
-							["<C-d>"] = actions.delete_buffer, -- delete in insert mode
+							["<C-h>"] = "which_key",
+							["<C-t>"] = trouble.open,
 						},
 						n = {
-							["<C-d>"] = actions.delete_buffer, -- delete anche in normal mode
-							["dd"] = actions.delete_buffer, -- mantieni anche 'dd' se ti piace
+							["<C-t>"] = trouble.open,
 						},
 					},
 				},
-			},
-			extensions = {
-				["ui-select"] = {
-					require("telescope.themes").get_dropdown(),
+				pickers = {
+					buffers = {
+						sort_mru = true,
+						ignore_current_buffer = true,
+						mappings = {
+							i = {
+								["<C-d>"] = actions.delete_buffer, -- delete in insert mode
+							},
+							n = {
+								["<C-d>"] = actions.delete_buffer, -- delete anche in normal mode
+								["dd"] = actions.delete_buffer, -- mantieni anche 'dd' se ti piace
+							},
+						},
+					},
 				},
+				extensions = {
+					["ui-select"] = {
+						require("telescope.themes").get_dropdown(),
+					},
+				},
+			})
+
+			pcall(require("telescope").load_extension, "fzf")
+			pcall(require("telescope").load_extension, "ui-select")
+
+			vim.keymap.set("n", "<leader>sh", builtin.help_tags, { desc = "[S]earch [H]elp" })
+			vim.keymap.set("n", "<leader>sk", builtin.keymaps, { desc = "[S]earch [K]eymaps" })
+			vim.keymap.set("n", "<leader>sf", builtin.find_files, { desc = "[S]earch [F]iles" })
+			vim.keymap.set("n", "<leader>ss", builtin.builtin, { desc = "[S]earch [S]elect Telescope" })
+			vim.keymap.set("n", "<leader>sw", builtin.grep_string, { desc = "[S]earch current [W]ord" })
+			vim.keymap.set("n", "<leader>sg", builtin.live_grep, { desc = "[S]earch by [G]rep" })
+			vim.keymap.set("n", "<leader>sd", builtin.diagnostics, { desc = "[S]earch [D]iagnostics" })
+			vim.keymap.set("n", "<leader>sr", builtin.resume, { desc = "[S]earch [R]esume" })
+			vim.keymap.set("n", "<leader>st", builtin.git_files, { desc = "[S]earch Gi[t]" })
+			vim.keymap.set("n", "<leader>s.", builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
+			vim.keymap.set("n", "<leader><leader>", builtin.buffers, { desc = "[ ] Find existing buffers" })
+			vim.keymap.set("n", "<leader>rr", builtin.lsp_references, { desc = "LSP: references" })
+		end,
+	},
+	{
+		"folke/trouble.nvim",
+		dependencies = { "nvim-tree/nvim-web-devicons" },
+		opts = {
+			focus = true,
+			win = { position = "left", size = 0.25 },
+			group = true,
+		},
+		cmd = "Trouble",
+		keys = {
+			{
+				"<leader>xx",
+				"<cmd>Trouble diagnostics toggle<cr>",
+				desc = "Diagnostics (Trouble)",
 			},
-		})
-
-		pcall(require("telescope").load_extension, "fzf")
-		pcall(require("telescope").load_extension, "ui-select")
-
-		vim.keymap.set("n", "<leader>sh", builtin.help_tags, { desc = "[S]earch [H]elp" })
-		vim.keymap.set("n", "<leader>sk", builtin.keymaps, { desc = "[S]earch [K]eymaps" })
-		vim.keymap.set("n", "<leader>sf", builtin.find_files, { desc = "[S]earch [F]iles" })
-		vim.keymap.set("n", "<leader>ss", builtin.builtin, { desc = "[S]earch [S]elect Telescope" })
-		vim.keymap.set("n", "<leader>sw", builtin.grep_string, { desc = "[S]earch current [W]ord" })
-		vim.keymap.set("n", "<leader>sg", builtin.live_grep, { desc = "[S]earch by [G]rep" })
-		vim.keymap.set("n", "<leader>sd", builtin.diagnostics, { desc = "[S]earch [D]iagnostics" })
-		vim.keymap.set("n", "<leader>sr", builtin.resume, { desc = "[S]earch [R]esume" })
-		vim.keymap.set("n", "<leader>st", builtin.git_files, { desc = "[S]earch Gi[t]" })
-		vim.keymap.set("n", "<leader>s.", builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
-		vim.keymap.set("n", "<leader><leader>", builtin.buffers, { desc = "[ ] Find existing buffers" })
-		vim.keymap.set("n", "<leader>rr", builtin.lsp_references, { desc = "LSP: references" })
-	end,
+		},
+	},
 }
