27a0fde1b5237bc5212ae9d740bc3ec3a771a525
Emanuele Caruso <emanuele.caruso@jethr.com>
2026-03-14 12:51:44 +0100

    chore: disable format on save in hugo projects

--- lua/plugins/format.lua
+++ lua/plugins/format.lua
@@ -13,10 +13,26 @@ return {
 	},
 	opts = {
 		notify_on_error = false,
-		format_on_save = {
-			lsp_fallback = false, -- non usare lsp formatting; vogliamo SOLO i formatter esterni
-			timeout_ms = 2000,
-		},
+		format_on_save = function(bufnr)
+			-- Disable format on save in HUGO projects
+			local bufname = vim.api.nvim_buf_get_name(bufnr)
+			local path = vim.fn.fnamemodify(bufname, ":p:h")
+
+			local hugo_config = vim.fn.findfile("hugo.toml", path .. ";")
+				or vim.fn.findfile("config.toml", path .. ";")
+				or vim.fn.findfile("hugo.yaml", path .. ";")
+				or vim.fn.findfile("config.yaml", path .. ";")
+				or vim.fn.finddir("archetypes", path .. ";")
+
+			if hugo_config ~= "" then
+				return nil
+			end
+
+			return {
+				lsp_fallback = false, -- non usare lsp formatting; vogliamo SOLO i formatter esterni
+				timeout_ms = 2000,
+			}
+		end,
 		-- Catene per filetype
 		formatters_by_ft = {
 			javascript = { "eslint_d", "prettierd" },
